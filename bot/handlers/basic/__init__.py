d8975dd2ce1a9ed6c3f0621adc575ac672fd2ee4
amtp1 <magomedov.abdul20012@gmail.com>
2024-02-06 23:35:59 +0300

    import mailing router

--- bot/handlers/basic/__init__.py
+++ bot/handlers/basic/__init__.py
@@ -1,5 +1,6 @@
 from .start import router_start
 from .download import router_download
 from .statistic import router_statistic
+from .mailing import router_mailing
 
-routers_basic = (router_start, router_statistic, router_download,)
+routers_basic = (router_start, router_statistic, router_mailing, router_download,)
